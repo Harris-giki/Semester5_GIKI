339b50ee80171fcc9f27c2445a2969a7d336a3ae
Muhammad Haris <harris.giki@gmail.com>
2025-09-15 22:34:05 +0500

    File Renamed

---
+++ Operating-Systems/codes/chapter5/pc1.c
@@ -0,0 +1,25 @@
+#include <stdio.h> // for using printf 
+#include <stdlib.h> // for exit
+#include <unistd.h> // for using fork and getpid()
+
+int main (int argc, char *argv[]) //argc (how many arg passes) and argv (array for actual arguments) are cmd-line arguments
+{
+    printf("hello (pid:%d)\n", (int) getpid());
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    }
+    else
+    {
+        printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
+    }
+    return 0
+
+}
