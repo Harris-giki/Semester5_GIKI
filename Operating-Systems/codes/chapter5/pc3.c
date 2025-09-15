1f3a4f9dd65ad3d597c1bd492a3719a0a11bf9ac
Muhammad Haris <harris.giki@gmail.com>
2025-09-16 00:00:36 +0500

    Book example 3

---
+++ Operating-Systems/codes/chapter5/pc3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello (pid:%d)\n", (int)getpid());
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);    
+    }
+    else if(rc == 0)
+    {
+        printf("hello, I am child (pid:%d)\n", (int)getpid());
+        char *myargs[3];
+        myargs[0] = strdup("wc"); // program: "wc" (word count)
+        myargs[1] = strdup("pc3.c"); // argument: file to count
+        myargs[2] = NULL; // marks end of array
+        execvp(myargs[0], myargs); // runs word count
+        printf("this shouldn't print out because execvp() is supposed to run wc\n");
+    }
+    else
+    {
+        int wc = wait(NULL);
+        printf("hello, I am parent of %d (wc:%d) (pid:%d)\n", rc, wc, (int)getpid());
+    }
+    return 0;
+}
